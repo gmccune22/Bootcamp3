ed09e7c4fb9bd2ad98707afd19b28b1aca2eca26
Yanna Lin <linyanna@yahoo.com>
2023-09-21 23:40:18 -0400

    cleanup

--- src/components/BuildingList.jsx
+++ src/components/BuildingList.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-
 function BuildingList( {data} ) {
   // TODO: Apply names filter on buildingList
   const buildingList = data
     .map(directory => {
       return (
-        // TODO: Create onClick listener to capture building id being selected
-        <tr key={directory.id}>
-          <td>{directory.code} </td>
-          <td> {directory.name} </td>
-        </tr>
+          // TODO: Create onClick listener to capture building id being selected
+          <tr key={directory.id}>
+            <td>{directory.code} </td>
+            <td> {directory.name} </td>
+          </tr>
       );
     });
 
-  return <div>{buildingList}</div>;
+  return <>{buildingList}</>;
 }
 
-export default BuilingList;
+export default BuildingList;
